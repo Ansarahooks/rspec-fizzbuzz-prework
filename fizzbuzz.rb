00d39200b19949a92e324b390d54c9d2d69e93e2
ansara.hooks <ansara.hooks@gmail.com>
2019-02-14 18:43:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz(int)
-  if int % 15 == 0
+def fizzbuzz(integer)
+  if integer % 15 == 0
   "FizzBuzz"
-  elsif int % 3 == 0
+elsif integer % 3 == 0
     "Fizz"
-  elsif int % 5 == 0
+  elsif integer % 5 == 0
     "Buzz"
   end
 end
